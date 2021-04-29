05c64e3f62eea573bbd253b4e23f913d4c5d084c
paugarrido <pau.garrido.7e3@itb.cat>
2021-04-29 16:31:51 +0200

    Second Commit

--- src/main/java/cat/itb/m09uf1/projectspringsecurity/model/entitat/Jugador.java
+++ src/main/java/cat/itb/m09uf1/projectspringsecurity/model/entitat/Jugador.java
@@ -1,21 +1,16 @@
 package cat.itb.m09uf1.projectspringsecurity.model.entitat;
 
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
+
 @Entity
 public class Jugador {
-
 
     @Id
     @NotNull
@@ -41,4 +36,63 @@ public class Jugador {
     @NotNull
     private int rate;
 
+    public Jugador() {
+    }
+
+    public Jugador(String name, String country, String league, String club, String position, int rate) {
+        this.name = name;
+        this.country = country;
+        this.league = league;
+        this.club = club;
+        this.position = position;
+        this.rate = rate;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getLeague() {
+        return league;
+    }
+
+    public void setLeague(String league) {
+        this.league = league;
+    }
+
+    public String getClub() {
+        return club;
+    }
+
+    public void setClub(String club) {
+        this.club = club;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
+
+    public int getRate() {
+        return rate;
+    }
+
+    public void setRate(int rate) {
+        this.rate = rate;
+    }
 }
